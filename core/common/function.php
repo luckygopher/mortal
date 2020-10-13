f7ebe04624675f0ed4c7f7e676c1d070979efa07
qingyunjun <houyaowen@medlinker.com>
2020-10-13 23:44:38 +0800

    增加获取随机字符串和跳转地址的函数

--- core/common/function.php
+++ core/common/function.php
@@ -1,1 +1,43 @@
 <?php
+/**
+ * Global helpers file with misc functions.
+ */
+
+/**
+ * 获取随机字符串
+ * @param int $randLength 长度
+ * @param int $addtime 是否加入当前时间戳
+ * @param int $includenumber 是否包含数字
+ * @return string
+ */
+if (! function_exists('rand_str')) {
+    function rand_str($randLength = 6, $addtime = 0, $includenumber = 0)
+    {
+        if ($includenumber) {
+            $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNPQEST123456789';
+        } else {
+            $chars = 'abcdefghijklmnopqrstuvwxyz';
+        }
+        $len = strlen($chars);
+        $randStr = '';
+        for ($i = 0; $i < $randLength; $i++) {
+            $randStr .= $chars[mt_rand(0, $len - 1)];
+        }
+        if ($addtime) {
+            $randStr .= time();
+        }
+        return $randStr;
+    }
+}
+
+/**
+ * 跳转地址
+ * @param $url
+ */
+if (! function_exists('rand_str')) {
+    function jump($url)
+    {
+        header('Location:'.$url);
+        exit();
+    }
+}
