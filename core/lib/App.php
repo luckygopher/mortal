e41abdc6c159fe11cf29b52258fe5d7c78e89ccb
qingyunjun <houyaowen@medlinker.com>
2020-10-16 23:04:21 +0800

    增加依赖注入容器

--- core/lib/App.php
+++ core/lib/App.php
@@ -1,1 +1,35 @@
 <?php
+/**
+ * @author Augus <mr_augus@qq.com>
+ * @category 依赖注入容器
+ */
+namespace core\lib;
+
+class App
+{
+    protected static $container = [];
+    /**
+     * 注入
+     *
+     * @param [type] $key
+     * @param [type] $val
+     * @return void
+     */
+    public static function set($key, $val)
+    {
+        self::$container[$key] = $val;
+    }
+    /**
+     * 获取
+     *
+     * @param [type] $key
+     * @return void
+     */
+    public static function get($key)
+    {
+        if (!array_key_exists($key, self::$container)) {
+            throw new Exception("not find".$key);
+        }
+        return self::$container[$key];
+    }
+}
