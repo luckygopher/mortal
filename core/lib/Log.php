7cac78a97d2b9449073cdf6fc122d10339a965e1
qingyunjun <mr.arvin.hou@gmail.com>
2020-10-31 11:10:17 +0800

    增加日志操作

--- core/lib/Log.php
+++ core/lib/Log.php
@@ -1,1 +1,24 @@
 <?php
+namespace core\lib;
+
+class Log
+{
+    /**
+     * 1.确定日志的存储方式
+     * 2.写日志
+     */
+    static $class;
+
+    static public function init()
+    {
+        //确定存储方式
+        $drive = Config::get('DRIVE','log');
+        $class = '\core\lib\drive\log\\'.$drive;
+        self::$class = new $class;
+    }
+
+    static public function log($message, $file = 'log')
+    {
+        self::$class->log($message, $file);
+    }
+}
