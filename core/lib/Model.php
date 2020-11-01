62c876fa0c3c2493763011d09bef3c20d6aa2bf1
qingyunjun <mr.arvin.hou@gmail.com>
2020-11-01 23:48:39 +0800

    增加数据库model

--- core/lib/Model.php
+++ core/lib/Model.php
@@ -1,1 +1,13 @@
 <?php
+namespace core\lib;
+
+use Medoo\Medoo;
+
+class Model extends Medoo
+{
+    public function __construct()
+    {
+        $dataConf = Config::all('database');
+        parent::__construct($dataConf);
+    }
+}
