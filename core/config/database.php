0bba7409552dd9569a00da41dbfa48df36e7063b
qingyunjun <mr.arvin.hou@gmail.com>
2020-11-10 00:29:56 +0800

    增加数据库配置

--- core/config/database.php
+++ core/config/database.php
@@ -1,1 +1,11 @@
 <?php
+return array(
+    'database_type' => 'mysql',
+    'database_name' => 'mortal',
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => 'xxxx',
+    'charset' => 'utf8',
+    'port' => 3306,
+    'prefix' => 'mortal_',
+);
